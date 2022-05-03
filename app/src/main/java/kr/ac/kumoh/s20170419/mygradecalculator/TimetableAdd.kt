76fc191ab8a5e9bfc0989f7d52efc7487c4c999d
gngnrkfl <84559902+gngnrkfl@users.noreply.github.com>
2022-05-03 21:55:32 +0900

    Update TimetableAdd.kt

--- app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/TimetableAdd.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/TimetableAdd.kt
@@ -13,7 +13,6 @@ class TimetableAdd : AppCompatActivity() {
     lateinit var binding : ActivityTimetableAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = ActivityMainBinding.inflate(layoutInflater)
         binding = ActivityTimetableAddBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -22,6 +21,16 @@ class TimetableAdd : AppCompatActivity() {
         val yearadapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, yeardata)
         binding.yearSpinner.adapter = yearadapter
 
-        //binding.yearSpinner.onItemSelectedListener = object:AdapterView.OnItemSelectedListener
+        val termdata:Array<String> = resources.getStringArray(R.array.semester)
+        val termadapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, termdata)
+        binding.termSpinner.adapter = termadapter
+
+        val areadata:Array<String> = resources.getStringArray(R.array.area)
+        val areaadapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, areadata)
+        binding.areaSpinner.adapter = areaadapter
+
+        val majordata:Array<String> = resources.getStringArray(R.array.major)
+        val majoradapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, majordata)
+        binding.majorSpinner.adapter = majoradapter
     }
 }
