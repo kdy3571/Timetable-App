79b27a6187ea3a541764149fa60ccfe29af29e90
kdy3571 <summer980531@gmail.com>
2022-05-03 21:24:23 +0900

    시간표 생성 부분 추가

--- app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/TimetableGeneration.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/TimetableGeneration.kt
@@ -2,10 +2,70 @@ package kr.ac.kumoh.s20170419.mygradecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import auto_schedule
+import kr.ac.kumoh.s20170419.mygradecalculator.databinding.ActivityTimetableGenerationBinding
 
 class TimetableGeneration : AppCompatActivity() {
+    lateinit var gbinding: ActivityTimetableGenerationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        var credit: Int
+        var name: String
+        var s_subject: String
+        val e_subject: String
+        var rest = ArrayList<Int>()
+        var ge: Int = 0
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_timetable_generation)
+        gbinding = ActivityTimetableGenerationBinding.inflate(layoutInflater)
+        setContentView(gbinding.root)
+
+        gbinding.button1.setOnClickListener {
+            //과목 리스트 불러오기
+            //불러온 과목들 s_subject에 추가
+        }
+
+        gbinding.button2.setOnClickListener {
+            //과목 리스트 불러오기
+            //불러온 과목들 e_subject에 추가
+        }
+
+        var listener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                when (buttonView.id) {
+                    R.id.check1 -> {
+                        rest.add(0)
+                    }
+                    R.id.check2 -> {
+                        rest.add(1)
+                    }
+                    R.id.check3 -> {
+                        rest.add(2)
+                    }
+                    R.id.check4 -> {
+                        rest.add(3)
+                    }
+                    R.id.check5 -> {
+                        rest.add(4)
+                    }
+                }
+            }
+
+            gbinding.check1.setOnCheckedChangeListener(listener)
+            gbinding.check2.setOnCheckedChangeListener(listener)
+            gbinding.check3.setOnCheckedChangeListener(listener)
+            gbinding.check4.setOnCheckedChangeListener(listener)
+            gbinding.check5.setOnCheckedChangeListener(listener)
+
+            gbinding.create.setOnClickListener {
+                auto_schedule(credit, s_subject, e_subject, rest, ge)
+            }
+        }
+
+        override fun onDestroy() {
+            gbinding = null super.onDestroy()
+        }
     }
 }
