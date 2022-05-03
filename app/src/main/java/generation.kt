9ad2b5cfb4c0ffe0348a87e5b2aac8cbb6f51bfe
kdy3571 <summer980531@gmail.com>
2022-05-03 22:01:18 +0900

    시간표 생성 부분 추가

--- app/src/main/java/generation.kt
+++ app/src/main/java/generation.kt
@@ -2,14 +2,14 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 class auto_schedule(var credit: Int, val s_subject: String,
-           val e_subject: String, var rest: Int, var ge: Int) {
+           val e_subject: String, var rest: ArrayList<Int>, var ge: Int) {
     fun main(args: Array<String>) {
         val slist = Array(5, { arrayOfNulls<String>(12) })
 
         //DB에서 전공, 교양, 필수 불러와서 배열 생성, 아직 생성 x
-        val em_list = arrayListOf<String>()
-        val ge_list = arrayListOf<String>()
-        val cs_list = arrayListOf<String>()
+        val em_list = arrayListOf<Int>()
+        val ge_list = arrayListOf<Int>()
+        val cs_list = arrayListOf<Int>()
 
         while (cs_list.isNotEmpty()) { // 배열에서 필수과목(division) 불러오기 4-1학기가 없을때까지
             //slist[DB.time/10][DB.time%10] = DB.code
@@ -17,11 +17,13 @@ class auto_schedule(var credit: Int, val s_subject: String,
             // 배열.delete(DB.code), code일부분이 일치 하는게 있으면?
         }
 
-        if (rest != 5) { // 공강일 존재
-            for (i in 0..11)
-                if (slist[rest][i] == null)
-                //종료, 공강일 불가능 반환
-                    slist[rest] = arrayOf("none") // 고쳐야함
+        if (rest.isNotEmpty()) { // 공강일 존재
+            for(i in rest) {
+                for (j in 0..11)
+                    if (slist[i][j] == null)
+                    //종료, 공강일 불가능 반환
+                slist[i] = arrayOf("none") // 고쳐야함
+            }
         }
 
         while (ge != 0) {
@@ -42,13 +44,14 @@ class auto_schedule(var credit: Int, val s_subject: String,
     }
 
     fun subject_add(
-        slist: ArrayList<String>,
-        list: Array<Array<String?>>,
+        list: ArrayList<Int>,
+        slist: Array<Array<String?>>,
         division: String,
         credit: Int
     ): Int {
         val random = Random()
         var num = random.nextInt(list.size)
+
         if (division == "교양") {
             if (credit - list[num] > 0) {// list로 불러온 과목의 학점 체크
                 // slist에 추가;
--- app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/TimetableGeneration.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/TimetableGeneration.kt
@@ -11,16 +11,15 @@ class TimetableGeneration : AppCompatActivity() {
     lateinit var gbinding: ActivityTimetableGenerationBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        var credit: Int
-        var name: String
-        var s_subject: String
-        val e_subject: String
-        var rest = ArrayList<Int>()
-        var ge: Int = 0
-
         super.onCreate(savedInstanceState)
         gbinding = ActivityTimetableGenerationBinding.inflate(layoutInflater)
         setContentView(gbinding.root)
+
+        var credit: Int = gbinding.creditInput.text.toString().toInt()
+        var s_subject: ArrayList<String>
+        val e_subject: ArrayList<String>
+        var rest = ArrayList<Int>()
+        var ge: Int = 0
 
         gbinding.button1.setOnClickListener {
             //과목 리스트 불러오기
@@ -32,40 +31,35 @@ class TimetableGeneration : AppCompatActivity() {
             //불러온 과목들 e_subject에 추가
         }
 
-        var listener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked) {
-                when (buttonView.id) {
-                    R.id.check1 -> {
-                        rest.add(0)
-                    }
-                    R.id.check2 -> {
-                        rest.add(1)
-                    }
-                    R.id.check3 -> {
-                        rest.add(2)
-                    }
-                    R.id.check4 -> {
-                        rest.add(3)
-                    }
-                    R.id.check5 -> {
-                        rest.add(4)
-                    }
-                }
-            }
-
-            gbinding.check1.setOnCheckedChangeListener(listener)
-            gbinding.check2.setOnCheckedChangeListener(listener)
-            gbinding.check3.setOnCheckedChangeListener(listener)
-            gbinding.check4.setOnCheckedChangeListener(listener)
-            gbinding.check5.setOnCheckedChangeListener(listener)
+//        var listener = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+//            if (isChecked) {
+//                when (buttonView.id) {
+//                    R.id.check1 -> rest.add(0)
+//                    R.id.check2 -> rest.add(1)
+//                    R.id.check3 -> rest.add(2)
+//                    R.id.check4 -> rest.add(3)
+//                    R.id.check5 -> rest.add(4)
+//                }
+//            }
+//            else {
+//                when (buttonView.id) {
+//                    R.id.check1 -> rest.removeAt(0)
+//                    R.id.check2 -> rest.removeAt(1)
+//                    R.id.check3 -> rest.removeAt(2)
+//                    R.id.check4 -> rest.removeAt(3)
+//                    R.id.check5 -> rest.removeAt(4)
+//                }
+//            }
+//
+//            gbinding.check1.setOnCheckedChangeListener(listener)
+//            gbinding.check2.setOnCheckedChangeListener(listener)
+//            gbinding.check3.setOnCheckedChangeListener(listener)
+//            gbinding.check4.setOnCheckedChangeListener(listener)
+//            gbinding.check5.setOnCheckedChangeListener(listener)
 
             gbinding.create.setOnClickListener {
                 auto_schedule(credit, s_subject, e_subject, rest, ge)
             }
-        }
-
-        override fun onDestroy() {
-            gbinding = null super.onDestroy()
         }
     }
 }
