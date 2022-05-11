be31b8cbf204b0cc36a68549122b3f0f4add7085
kdy3571 <summer980531@gmail.com>
2022-05-12 01:26:53 +0900

    2022/05/12

--- app/src/main/java/generation.kt
+++ app/src/main/java/generation.kt
@@ -32,6 +32,7 @@ fun auto_schedule(
     while (ge[0] != 0) {
         if (subject_add(ge_list, slist, "교양", credit) == 1)
             ge[0] = ge[0] - 1
+        Log.d("Ge", ge[0].toString())
     }
 
 //    while (credit != 0) {
@@ -58,14 +59,29 @@ fun subject_add(
     if (division == "교양") {
         if (credit[0] - list[num].credit.toInt() > 0) {// list로 불러온 과목의 학점 체크
             time = list[num].time.split(",")
+            Log.d("time", time.toString())
+            Log.d("code", list[num].code)
+            Log.d("log", log10(time[0].toDouble()).toString())
             for (t in time) {
-                if(log10(t.toDouble()).toInt() != 3) {
-                    slist[t.toInt() / 10][t.toInt() % 10] = list[num].code
-                    slist[t.toInt() / 10][t.toInt() % 10]?.let { Log.i("slist", it) }
-                }
-                else {
-                    slist[t.toInt() / 100][t.toInt() % 100] = list[num].code
-                    slist[t.toInt() / 100][t.toInt() % 100]?.let { Log.i("slist", it) }
+                Log.d("t", t)
+                if (log10(t.toDouble()).toInt() + 1 != 3) {
+                    if (slist[t.toInt() / 10][t.toInt() % 10] == null) {
+                        slist[t.toInt() / 10][t.toInt() % 10] = list[num].code
+                        slist[t.toInt() / 10][t.toInt() % 10]?.let { Log.d("slist", it) }
+                    }
+                    else {
+                        list.removeAt(num)
+                        return 0
+                    }
+                } else {
+                    if (slist[t.toInt() / 10][t.toInt() % 10] == null) {
+                        slist[t.toInt() / 100][t.toInt() % 100] = list[num].code
+                        slist[t.toInt() / 100][t.toInt() % 100]?.let { Log.i("slist", it) }
+                    }
+                    else {
+                        list.removeAt(num)
+                        return 0
+                    }
                 }
             }
             return 1
@@ -74,15 +90,17 @@ fun subject_add(
             // list[num] 삭제
         } else {
             //list[num] 삭제
-        }
-    } else if (division == "전공") {
-        if (credit[0] - list[num].credit.toInt() > 0) {// list로 불러온 과목의 학점 체크
-            // slist에 추가;
-            // credit -= list[num].credit
-            // list[num] 삭제
-        } else {
-            //list[num] 삭제
+            return 0
         }
     }
-    return 0
+//    } else if (division == "전공") {
+//        if (credit[0] - list[num].credit.toInt() > 0) {// list로 불러온 과목의 학점 체크
+//            // slist에 추가;
+//            // credit -= list[num].credit
+//            // list[num] 삭제
+//        } else {
+//            //list[num] 삭제
+//        }
+//    }
+    return 1
 }
