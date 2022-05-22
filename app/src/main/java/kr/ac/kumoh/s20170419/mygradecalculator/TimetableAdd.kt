b9028f3837bce9e7ffcd4da3a5961fe6091f19d3
gngnrkfl <gngnrkfl>
2022-05-22 23:26:37 +0900

    내부 데이터베이스 제작중

--- app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/TimetableAdd.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/TimetableAdd.kt
@@ -1,55 +1,48 @@
 package kr.ac.kumoh.s20170419.mygradecalculator
+
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.graphics.Color
+import android.icu.util.Calendar
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_timetable_add.*
-import kotlinx.android.synthetic.main.listdesign.*
-import kotlinx.android.synthetic.main.timetable_layout.*
-import kotlinx.android.synthetic.main.timetable_layout.view.*
-import kotlinx.coroutines.Delay
-import kotlinx.coroutines.delay
 import kr.ac.kumoh.s20170419.mygradecalculator.databinding.ActivityTimetableAddBinding
-import java.lang.Thread.sleep
 
 class TimetableAdd : MainActivity() {
-    lateinit var binding : ActivityTimetableAddBinding
+    lateinit var binding: ActivityTimetableAddBinding
     private val model: ViewModel by viewModels()
     private lateinit var dbadapter: DatabaseAdapter
+    var innerdb: ScheduleDatabase? = null
+
     @SuppressLint("SetTextI18n", "NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTimetableAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val yeardata:Array<String> = resources.getStringArray(R.array.grade)
+        val yeardata: Array<String> = resources.getStringArray(R.array.grade)
         val yearadapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, yeardata)
         binding.yearSpinner.adapter = yearadapter
 
-        val termdata:Array<String> = resources.getStringArray(R.array.semester)
+        val termdata: Array<String> = resources.getStringArray(R.array.semester)
         val termadapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, termdata)
         binding.termSpinner.adapter = termadapter
 
-        val areadata:Array<String> = resources.getStringArray(R.array.area)
+        val areadata: Array<String> = resources.getStringArray(R.array.area)
         val areaadapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, areadata)
         binding.areaSpinner.adapter = areaadapter
 
-        val majordata:Array<String> = resources.getStringArray(R.array.major)
+        val majordata: Array<String> = resources.getStringArray(R.array.major)
         val majoradapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, majordata)
         binding.majorSpinner.adapter = majoradapter
 
-        val search_button : Button = findViewById(R.id.search_button)
+        val search_button: Button = findViewById(R.id.search_button)
 
         dbadapter = DatabaseAdapter(model) { subject -> adapterOnClick(subject) }
         binding.listdata.apply {
@@ -67,24 +60,44 @@ class TimetableAdd : MainActivity() {
             val term: String = termSpinner.selectedItem.toString()
             val area: String = areaSpinner.selectedItem.toString()
             val major: String = majorSpinner.selectedItem.toString()
-            if(major == "컴퓨터공학과")
+            if (major == "컴퓨터공학과")
                 model.requestList("금오공과대학교", year, term, area)
         }
     }
 
-    private fun adapterOnClick(subjectdata: ViewModel.Subject):Unit {
+    private fun adapterOnClick(subjectdata: ViewModel.Subject): Unit {
         val dlg = kr.ac.kumoh.s20170419.mygradecalculator.Dialog(this)
         val iintent = Intent(this, MainActivity::class.java)
+        innerdb = ScheduleDatabase.getDatabase(this)!!
         dlg.dialog()
-        dlg.setOnClickedListener(object : kr.ac.kumoh.s20170419.mygradecalculator.Dialog.ButtonClickListener{
+        dlg.setOnClickedListener(object :
+            kr.ac.kumoh.s20170419.mygradecalculator.Dialog.ButtonClickListener {
             override fun onClicked(data: Int) {
-                if(data == 1) {
+                if (data == 1) {
+                    Thread(Runnable {
+                        val subj = weekstate(
+                            0,
+                            subjectdata.college,
+                            subjectdata.subject,
+                            subjectdata.name,
+                            subjectdata.professor,
+                            subjectdata.code,
+                            subjectdata.room,
+                            subjectdata.time,
+                            subjectdata.division,
+                            subjectdata.credit,
+                            subjectdata.grade,
+                            subjectdata.semester
+                        )
+                        innerdb?.weekDao()?.insert(subj)
+                        val data = innerdb?.weekDao()?.getAll()
+                        Log.d("database", data.toString())
+                    }).start()
                     iintent.putExtra("name", subjectdata.name)
                     iintent.putExtra("time", subjectdata.time)
                     finish()
                     startActivity(iintent)
-                }
-                else if(data == 0)
+                } else if (data == 0)
                     Toast.makeText(getApplication(), "취소당", Toast.LENGTH_LONG).show()
             }
         })
