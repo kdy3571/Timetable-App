8cbe727301cc6513d073857c7dab16d7264a6d4b
Dohyeon_17 <pak981116@gmail.com>
2022-05-24 22:52:33 +0900

    자동생성설정완료, 로그인UI개선

--- app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/loginActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/loginActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.kumoh.s20170419.mygradecalculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kr.ac.kumoh.s20170419.mygradecalculator.databinding.ActivityLoginBinding
 import kr.ac.kumoh.s20170419.mygradecalculator.databinding.ActivityMainBinding
 
@@ -16,6 +17,22 @@ class loginActivity : AppCompatActivity() {
 
         view = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(view.root)
+
+        val select_college_data:Array<String> = resources.getStringArray(R.array.college_select)
+        val select_college_data_apter = ArrayAdapter(this, android.R.layout.simple_list_item_1, select_college_data)
+        view.collegeSelect.adapter = select_college_data_apter
+
+        val select_major_data:Array<String> = resources.getStringArray(R.array.major_select)
+        val select_major_data_apter = ArrayAdapter(this, android.R.layout.simple_list_item_1, select_major_data)
+        view.maiorSelect.adapter = select_major_data_apter
+
+        val select_grade_data:Array<String> = resources.getStringArray(R.array.grade_select)
+        val select_grade_data_apter = ArrayAdapter(this, android.R.layout.simple_list_item_1, select_grade_data)
+        view.gradeSelect.adapter = select_grade_data_apter
+
+        val select_semester_data:Array<String> = resources.getStringArray(R.array.semester_select)
+        val select_semester_data_apter = ArrayAdapter(this, android.R.layout.simple_list_item_1, select_semester_data)
+        view.semesterSelect.adapter = select_semester_data_apter
 
         view.loginButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
