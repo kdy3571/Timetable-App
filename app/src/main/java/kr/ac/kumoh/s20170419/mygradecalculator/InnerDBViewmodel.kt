7d57efbf40cccf0ce715cbc63b1f015862bfd6d9
kdy3571 <summer980531@gmail.com>
2022-05-29 21:09:08 +0900

    2022/05/28 DB 수정

--- app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/InnerDBViewmodel.kt
+++ app/src/main/java/kr/ac/kumoh/s20170419/mygradecalculator/InnerDBViewmodel.kt
@@ -6,8 +6,7 @@ import androidx.lifecycle.AndroidViewModel
 
 class InnerDBViewmodel(context: Application) : AndroidViewModel(context) {
     private val weekdb = ScheduleDatabase.getDatabase(context)!!.weekDao()
-    val name: ArrayList<String> = arrayListOf()
-    val time: ArrayList<String> = arrayListOf()
+    private val gpdb = GPDatabase.getDatabase(context)!!.gpDao()
 //    var red: Int = 0
 //    var blue: Int = 0
 //    var green: Int = 0
@@ -33,6 +32,26 @@ class InnerDBViewmodel(context: Application) : AndroidViewModel(context) {
             subject.semester
         )
         weekdb.insert(data)
+    }
+
+    fun connect(db: weekstate) {
+        val data = gpstate(
+            0,
+            db.gs,
+            db.subject,
+            db.name,
+            db.credit,
+            "A+"
+        )
+        gpdb.insert(data)
+    }
+
+    fun getInfo(gs: String): List<gpstate> {
+        return gpdb.getInfo(gs)
+    }
+
+    fun delInfo(gs: String) {
+        gpdb.delete(gs)
     }
 
     fun getall(gs: String): List<weekstate> {
